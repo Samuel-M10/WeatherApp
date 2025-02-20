3ff11208148e0d45940e65976e390a22383a64cd
samuel <samuelmisra777@gmail.com>
2025-02-20 13:51:45 +0530

    Create Weather App

---
+++ assets/js/route.js
@@ -0,0 +1,38 @@
+'use strict';
+import {updateWeather, error404} from "./app.js";
+const defaultLocation="#/weather?lat=51.509865&lon=-0.118092";
+const currentLocation= function(){
+    window.navigator.geolocation.getCurrentPosition(res=>{
+        const{latitude, longitude}=res.coords;
+        console.log(res.coords)
+
+        updateWeather(`lat=${latitude}, Lon=${longitude}`);
+    },  err => {
+            window.location.hash= defaultLocation;
+    });
+}
+/**
+ * 
+ * @param {string} query Searched query
+ */
+const searchedLocation = query=> updateWeather(...query.split("&"));
+//updateWeather("lat=51.509865", "lon=-0.118092")
+
+const routes = new Map([
+    ["/current-location", currentLocation],
+    ["/weather", searchedLocation]
+]);
+const checkHash=function(){
+    const requestURL = window.location.hash.slice(1);
+    const [route,query] = requestURL.includes ? requestURL.split("?"):[requestURL];
+    routes.get(route) ? routes.get(route)(query): error404();
+}
+window.addEventListener("hashchange", checkHash);
+window.addEventListener("load", function(){
+    if(!this.window.location.hash){
+        window.location.hash="#/current-location";
+    }
+    else{
+        checkHash();
+    }
+})
